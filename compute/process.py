12706daedf4d1de82a71bf8a162a63d876e1e3f1
Sankha Narayan Guria <sankha93@gmail.com>
2013-01-23 17:17:56 +0530

    Compute bad postures on the computer

---
+++ compute/process.py
@@ -0,0 +1,32 @@
+# Code to do the processing of wrong posture across a timeframe
+
+import csv, serial
+
+# initializing some stuff
+ser = serial.Serial("/dev/ttyACM0")
+csvfile = open("data.csv", 'wb')
+csvwriter = csv.writer(csvfile, delimiter = ',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+csvwriter.writerow(["sl_no", "flexVal", "buzzed"])
+
+num = 1 # reference value for first reading
+counter = 0 # initial value of counter
+limit = 10 # tolerance value for counter
+threshold = 410 # threshold value, below this we should buzz
+
+# main program loop
+while(ser.isOpen()):
+  val = int(ser.readline()[:3])
+  # track the bad posture
+  if(val > threshold):
+    counter = counter + 2
+    
+    # if the bad posture is prolonged, send the buzz
+    if(counter > limit):
+      serial.write("1")
+      counter = 0
+  # if the posture is good, then reverse the effect of bad posture
+  else:
+    if(counter > 0)
+      counter = counter - 1
+
+ser.close()
